ef8ff19de8d4f09f679c189b5663c6e6cebdda88
kaanyuxel <kaan.oyulmaz@gmail.com>
2022-10-06 08:41:55 +0300

    Updates for idle and fight

--- Player.py
+++ Player.py
@@ -6,11 +6,12 @@ class Player(pygame.sprite.Sprite):
     def __init__(self,group):
         super().__init__(group)
         self.character = Character.Character()
-        self.image = self.character.walk_down()[0]
+        self.image = self.character.walk_down[0]
         self.rect = self.image.get_rect(center = (512, 512))
         
         self.walkCount = 0
         self.attack_frame = 0
+        self.idle_count = 0
         self.face = "down"
         self.test_keyboard = True
 
@@ -34,25 +35,25 @@ class Player(pygame.sprite.Sprite):
             self.face_update()    
             self.left_click = False
         
-        keys = pygame.key.get_pressed()     
-        if not keys[pygame.K_f] and not keys[pygame.K_r]:    
-            if self.test_keyboard:            
-                if keys[pygame.K_UP]:
-                    self.face = "up"  
-                    self.target = pygame.Vector2(self.rect.centerx , self.rect.centery - 1*self.speed)
-                elif keys[pygame.K_DOWN]:
-                    self.face = "down"
-                    self.target = pygame.Vector2(self.rect.centerx, self.rect.centery + 1*self.speed)
-                elif keys[pygame.K_RIGHT]:     
-                    self.face = "right"
-                    self.target = pygame.Vector2(self.rect.centerx + 1*self.speed, self.rect.centery)
-                elif keys[pygame.K_LEFT]:
-                    self.face = "left"
-                    self.target = pygame.Vector2(self.rect.centerx - 1*self.speed, self.rect.centery)
+        keys = pygame.key.get_pressed()        
+        if self.test_keyboard:            
+            if keys[pygame.K_UP]:
+                self.face = "up"  
+                self.target = pygame.Vector2(self.rect.centerx , self.rect.centery - 1*self.speed)
+            elif keys[pygame.K_DOWN]:
+                self.face = "down"
+                self.target = pygame.Vector2(self.rect.centerx, self.rect.centery + 1*self.speed)
+            elif keys[pygame.K_RIGHT]:     
+                self.face = "right"
+                self.target = pygame.Vector2(self.rect.centerx + 1*self.speed, self.rect.centery)
+            elif keys[pygame.K_LEFT]:
+                self.face = "left"
+                self.target = pygame.Vector2(self.rect.centerx - 1*self.speed, self.rect.centery)
         
     def update(self):
         self.input()
-        self.attack()
+        if self.attacking == True:
+            self.attack()
         self.movement()
 
 
@@ -77,14 +78,15 @@ class Player(pygame.sprite.Sprite):
         
         if move_length < self.speed:
             self.pos = self.target
-            self.pos = self.rect.center 
-            #self.animate_walk(0)
+            self.pos = self.rect.center
+            if self.attacking == False: 
+                self.animate_idle()
         elif move_length != 0:
             self.walkCount += 1
             move.normalize_ip()
             move = move * self.speed
             self.pos += move   
-            self.animate_walk(self.walkCount//7)
+            self.animate_walk(self.walkCount//5)
             if self.attacking == True:
                 self.attack() 
 
@@ -100,49 +102,62 @@ class Player(pygame.sprite.Sprite):
     def attack(self):
         if self.fight_1 or self.fight_2:
             self.attack_frame += 1
-            if self.fight_1:                 
+            if self.fight_1:          
+                self.animate_fight(1, self.attack_frame//6)       
                 if self.attack_frame > 30:
                     self.attack_frame = 0
                     self.fight_1 = False
                     self.attacking = False
-                self.animate_fight(1, self.attack_frame//6)
                 
             if self.fight_2:
                 if self.attack_frame > 30:
                     self.attack_frame = 0
                     self.fight_2 = False
                     self.attacking = False
-                self.animate_fight(2, self.attack_frame//6)
-        else:
-            self.animate_walk(0)                          
-        
+                self.animate_fight(2, self.attack_frame//6)                      
+    
+    def animate_idle(self):
+        self.idle_count += 1
+        if self.idle_count >= 60:
+            self.idle_count = 0
+
+        if self.face == "right":
+            self.image = self.character.idle_right[self.idle_count//12]
+        if self.face == "left":
+            self.image = self.character.idle_left[self.idle_count//12]
+        if self.face == "up":
+            self.image = self.character.idle_up[self.idle_count//12]
+        if self.face == "down":
+            self.image = self.character.idle_down[self.idle_count//12]
+        self.rect = self.image.get_rect(center = self.rect.center)        
+
     def animate_walk(self, walk_count): 
         if self.face == "right":
-            self.image = self.character.walk_right()[walk_count]
+            self.image = self.character.walk_right[walk_count]
         if self.face == "left":
-            self.image = self.character.walk_left()[walk_count]
+            self.image = self.character.walk_left[walk_count]
         if self.face == "up":
-            self.image = self.character.walk_up()[walk_count]
+            self.image = self.character.walk_up[walk_count]
         if self.face == "down":
-            self.image = self.character.walk_down()[walk_count]
+            self.image = self.character.walk_down[walk_count]
         self.rect = self.image.get_rect(center = self.rect.center)  
 
     def animate_fight(self, fight_type, fight_count):
         if fight_type == 1:
             if self.face == "left":
-                self.image = self.character.fight_1_left()[fight_count]
+                self.image = self.character.fight_1_left[fight_count]
             if self.face == "right":
-                self.image = self.character.fight_1_right()[fight_count]    
+                self.image = self.character.fight_1_right[fight_count]    
             if self.face == "up":
-                self.image = self.character.fight_1_up()[fight_count]
+                self.image = self.character.fight_1_up[fight_count]
             if self.face == "down":
-                self.image = self.character.fight_1_down()[fight_count]
+                self.image = self.character.fight_1_down[fight_count]
         if fight_type == 2:
             if self.face == "left":
-                self.image = self.character.fight_2_left()[fight_count]
+                self.image = self.character.fight_2_left[fight_count]
             if self.face == "right":
-                self.image = self.character.fight_2_right()[fight_count]    
+                self.image = self.character.fight_2_right[fight_count]    
             if self.face == "up":
-                self.image = self.character.fight_2_up()[fight_count]
+                self.image = self.character.fight_2_up[fight_count]
             if self.face == "down":
-                self.image = self.character.fight_2_down()[fight_count]                                         
+                self.image = self.character.fight_2_down[fight_count]                                         
